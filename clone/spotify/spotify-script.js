05f4e586a8cc1937cd673ce7d1c18eac8426d425
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-09 10:30:09 +0530

    update file

--- clone/spotify/spotify-script.js
+++ clone/spotify/spotify-script.js
@@ -1,18 +1,31 @@
 console.log("Welcome to spotify");
 
 let songIndex=0;
-let audioElement=new Audio('1.mp3');
+let audioElement=new Audio('songs/1.mp3');
 let masterplay=document.getElementById('masterPlay');
 let myprogressBar=document.getElementById('myProgressBar');
-
-let songs=[
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "salam-e-Ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+let gif=document.getElementById('gif');
+let songitems=Array.from(document.getElementById('songitem'));
+ 
+let songs = [
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
+
+
+songitems.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementByClassName('songitem');
+})
+
 
 // audioElement.play();
 
@@ -20,15 +33,27 @@ let songs=[
 masterplay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
+        masterplay.classList.remove('far fa-3x fa-play-circle');
+        masterplay.classList.add('far fa-3x fa-pause-circle')
+    }
+    else{
+        audioElement.pause();
+        masterplay.classList.remove('far fa-3x fa-pause-circle');
+        masterplay.classList.add('far fa-3x fa-play-circle')
     }
 })
 
 
 //Listen to Events
-myprogressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
     console.log('timeupdate')
+    //update seekbar
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress);
+    myprogressBar.value=progress;
 })
 
-
-
+myprogressBar.addEventListener('change',()=>{
+    audioElement.currentTime=myprogressBar.value*audioElement;
+})
 
