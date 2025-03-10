d2f921220d6ad26b510bfd9d58947776e3ee21e2
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-10 22:50:27 +0530

    update file

--- clone/spotify/spotify-script.js
+++ clone/spotify/spotify-script.js
@@ -58,3 +58,53 @@ myprogressBar.addEventListener('change',()=>{
     audioElement.currentTime=myprogressBar.value*audioElement;
 })
 
+const makeAllplays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove("fa-pause-circle");
+        element.classList.add("fa-play-circle");
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllplays();
+        songIndex=parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle')
+        e.target.classList.add('fa-pause-circle')
+        audioElement.src=`songs/${songIndex+1}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterplay.classList.remove('fa-play-circle')
+        masterplay.classList.add('fa-pause-circle')
+
+    })
+})
+
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>9){
+        songIndex=0
+    }else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle')
+    masterplay.classList.add('fa-pause-circle')
+})
+
+
+
+
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=0
+    }else{
+        songIndex-=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle')
+    masterplay.classList.add('fa-pause-circle')
+})
